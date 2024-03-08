a965205332ad58d9bae4e087d69ecdad95313961
ryzhovalex <thed4rkof@gmail.com>
2024-03-08 07:45:34 +0300

    2024-03-08T07:45:30,026015751+03:00

--- projects/server/src/app.py
+++ projects/server/src/app.py
@@ -1,6 +1,10 @@
 import asyncio
+import os
+import sys
 from pykit.log import log
+from pykit.env import EnvUtils
 from typing import Self
+from loguru import logger as _logger
 import aioconsole
 
 class App:
@@ -10,6 +14,18 @@ class App:
         await app.serve()
 
     async def init(self) -> Self:
+        env_debug = EnvUtils.get_bool("FORT_DEBUG", "1")
+        self._is_debug = env_debug
+        log.is_debug = env_debug
+
+        _logger.remove(0)
+        log_format = \
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> |" \
+            " <level>{level: <8}</level> - <level>{message}</level>"
+        _logger.add(sys.stdout, format=log_format)
+        log.debug("hello?")
+        exit(0)
+
         return self
 
     async def serve(self):
